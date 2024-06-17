4dc582462f295f7da06f3b8e242f6b99a0f79f02
allenwa2_cisco <allenwa2@cisco.com>
2024-06-17 16:36:15 -0400

    added new test

--- CSharpCodeReview/Calculator.cs
+++ CSharpCodeReview/Calculator.cs
@@ -15,6 +15,22 @@ public class Calculator
         // Perform subtraction
         return a - b;
     }
+
+       public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return (double)a / b;
+        }
+
+    
 }
 
 }
